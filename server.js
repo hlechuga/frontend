47d2bc5e74934670282603d049921d06202244d3
Harrold Lechuga <harrold.lechuga@asia.ing.com>
2021-02-04 15:49:19 +0800

    Added express server for serving application

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+var process = require('process')
+
+app.use(express.static(path.join(__dirname, 'build')));
+
+// START: FOR DEVELOPMENT ONLY
+// var cors = require('cors')
+// app.use(cors());
+// const { createProxyMiddleware } = require('http-proxy-middleware');
+// app.use('/api', createProxyMiddleware({ 
+//     target: 'http://localhost:8080/',
+//     changeOrigin: true, 
+//     onProxyRes: function (proxyRes, req, res) {
+//        proxyRes.headers['Access-Control-Allow-Origin'] = '*';
+//     }
+// }));
+// END: FOR DEVELOPMENT ONLY
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+process.on('SIGINT', () => {
+  console.info("Interrupted")
+  process.exit(0)
+})
+
+app.listen(5000);
